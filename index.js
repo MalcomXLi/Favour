7e5e28f60a8827f314b448ccde4393cd3581e9d0
Jonathan Wen <jonathanwen95@gmail.com>
2016-09-17 18:45:58 -0400

    added route

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ var port = process.env.PORT || 8000;
 
 var app = express();
 
-// var connectionString = process.env.DATABASE_URL || 'postgresql://root@104.196.183.161:26257?sslmode=disable';
-// var client = new pg.Client(connectionString);
+var connectionString = process.env.DATABASE_URL || 'postgresql://root@104.196.183.161:26257?sslmode=disable';
+var client = new pg.Client(connectionString);
+console.log(connectionString);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -19,44 +20,44 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-// app.get('/test', function(req, res) {
-//   try{
-//     // Get a Postgres client from the connection pool
-//     pg.connect(connectionString, function(err, client, done) {
-//       // Handle connection errors
-//       if(err) {
-//         done();
-//         console.log(err);
-//         return res.status(500).json({ success: false, data: err});
-//       }
+app.get('/test', function(req, res) {
+  try{
+    // Get a Postgres client from the connection pool
+    pg.connect(connectionString, function(err, client, done) {
+      // Handle connection errors
+      if(err) {
+        done();
+        console.log(err);
+        return res.status(500).json({ success: false, data: err});
+      }
 
-//       var accountTable = '<table class="table table-striped table-bordered"><tr><th>User</th><th>Balance</th></tr>';
+      var accountTable = '<table class="table table-striped table-bordered"><tr><th>User</th><th>Balance</th></tr>';
 
-//       var query = client.query("SELECT name, balance FROM bank.accounts;");
+      var query = client.query("SELECT * FROM users.accounts;");
 
-//       query.on('row', function(row) {
-//         accountTable = accountTable + '<tr><td>' + row.name + '</td><td>' + row.balance +' </td></tr>';
-//        });
+      query.on('row', function(row) {
+        accountTable = accountTable + '<tr><td>' + row.id + '</td><td>' + row.name +' </td></tr>';
+       });
 
-//       query.on('end', function() {
-//         accountTable += "</table>";
-//         if (err) {
-//           throw (err);
-//         }
-//         done();
-//         res.send(accountTable);
-//       });
+      query.on('end', function() {
+        accountTable += "</table>";
+        if (err) {
+          throw (err);
+        }
+        done();
+        res.send(accountTable);
+      });
 
-//       query.on('error', function(err) {
-//         console.log(err);
-//         res.status(500).json({ success: false, data: err});
-//         done();
-//       });
-//     });
-//   } catch(ex) {
-//     console.log('error', ex);
-//   }
-// });
+      query.on('error', function(err) {
+        console.log(err);
+        res.status(500).json({ success: false, data: err});
+        done();
+      });
+    });
+  } catch(ex) {
+    console.log('error', ex);
+  }
+});
 
 var server = app.listen(port, function (cb) {
   var port = server.address().port;
